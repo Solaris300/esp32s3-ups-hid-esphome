921fa930fca732a73aac2cfddb71fa7e25f48f46
Solaris300 <154632360+Solaris300@users.noreply.github.com>
2025-10-18 11:35:38 +0200

    Update ups_hid.cpp

--- components/ups_hid/ups_hid.cpp
+++ components/ups_hid/ups_hid.cpp
@@ -1,1 +1,18 @@
+#include "ups_hid.h"
+
+namespace esphome {
+namespace ups_hid {
+
+static const char *const TAG = "ups_hid";
+
+void UpsHid::setup() {
+  ESP_LOGI(TAG, "UPS HID component started (hello from ESPHome external component).");
+}
+
+void UpsHid::loop() {
+  // Aquí más adelante gestionaremos eventos del USB Host y publicaremos sensores.
+}
+
+}  // namespace ups_hid
+}  // namespace esphome
 
