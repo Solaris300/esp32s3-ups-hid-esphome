cde5f83fdd8aac6a5bf50d78514dda664026ebc9
Solaris300 <154632360+Solaris300@users.noreply.github.com>
2025-10-18 16:17:04 +0200

    Update __init__.py

--- components/ups_hid/__init__.py
+++ components/ups_hid/__init__.py
@@ -3,12 +3,12 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 ups_hid_ns = cg.esphome_ns.namespace('ups_hid')
-UpsHid = ups_hid_ns.class_('UpsHid', cg.Component)
+UpsHid = ups_hid_ns.class_('UpsHid', cg.PollingComponent)  # <-- PollingComponent
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UpsHid),
-}).extend(cv.COMPONENT_SCHEMA)   # <-- importante
+}).extend(cv.polling_component_schema("1s"))  # <-- intervalo por defecto
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)  # <-- pasa 'config' (no vacío)
+    await cg.register_component(var, config)
