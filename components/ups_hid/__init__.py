ec56da04530b918381d972089ad5a2db8571cb4f
Solaris300 <154632360+Solaris300@users.noreply.github.com>
2025-10-18 16:02:16 +0200

    Update __init__.py

--- components/ups_hid/__init__.py
+++ components/ups_hid/__init__.py
@@ -7,9 +7,8 @@ UpsHid = ups_hid_ns.class_('UpsHid', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UpsHid),
-})
+}).extend(cv.COMPONENT_SCHEMA)   # <-- importante
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
+    await cg.register_component(var, config)  # <-- pasa 'config' (no vacío)
