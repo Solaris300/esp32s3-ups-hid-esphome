6e47edb058684d2ef08df1321cd8e9fc4919fd7a
Solaris300 <154632360+Solaris300@users.noreply.github.com>
2025-10-18 16:43:15 +0200

    Update ups_hid.h

--- components/ups_hid/ups_hid.h
+++ components/ups_hid/ups_hid.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "usb/usb_host.h"   // <-- Host Library base
 
 namespace esphome {
 namespace ups_hid {
 
-class UpsHid : public PollingComponent {  // <-- PollingComponent
+class UpsHid : public PollingComponent {
  public:
   void setup() override;
   void dump_config() override;
-  void update() override;  // <-- en vez de loop()
+  void update() override;
+
  private:
   bool hello_logged_{false};
+  static void host_daemon_task_(void *arg);
 };
 
 }  // namespace ups_hid
