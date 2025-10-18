9dd82b5455a06c7b9b3cee66a9301f32ca6054e3
Solaris300 <154632360+Solaris300@users.noreply.github.com>
2025-10-18 11:35:21 +0200

    Update ups_hid.h

--- components/ups_hid/ups_hid.h
+++ components/ups_hid/ups_hid.h
@@ -1,1 +1,17 @@
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ups_hid {
+
+class UpsHid : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
+};
+
+}  // namespace ups_hid
+}  // namespace esphome
 
